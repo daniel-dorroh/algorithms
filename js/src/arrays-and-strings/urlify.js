b9305d4a569b0821738e992212f036ad9c9e066b
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-14 21:48:45 -0500

    Implemented urlify

---
+++ js/src/arrays-and-strings/urlify.js
@@ -0,0 +1,75 @@
+// Write a method to replace all spaces in a
+// string with '%20'. You may assume that the
+// string has sufficient space at the end to
+// hold the additional characters, and that you
+// are given the "true" length of the string.
+
+const indexOfLastNonWhiteSpaceCharacter = (input, length) => {
+  let i = length - 1;
+  while (true && i > 0) {
+    if (input[i] !== ' ') {
+      break;
+    }
+    i--;
+  }
+  return i;
+};
+
+// The question sets the stage for an in-place,
+// space-optimized solution, but since strings are
+// immutable in JavaScript, I'm using arrays for
+// a 'c-like' solution.
+export const cStyleUrlify = (charArray, length) => {
+  let writePosition = length - 1;
+  let readPosition =
+      indexOfLastNonWhiteSpaceCharacter(charArray, length);
+  if (readPosition === 0) {
+    writePosition = 0;
+    while (writePosition < length) {
+      charArray[writePosition++] = '%';
+      charArray[writePosition++] = '2';
+      charArray[writePosition++] = '0';
+    }
+  } else {
+    while (readPosition >= 0) {
+      if (charArray[readPosition] !== ' ') {
+        charArray[writePosition] = charArray[readPosition];
+        writePosition--;
+      } else {
+        charArray[writePosition--] = '0';
+        charArray[writePosition--] = '2';
+        charArray[writePosition--] = '%';
+      }
+      readPosition--;
+    }
+  }
+  return charArray;
+};
+
+// A non-c-like implementation
+export const urlify = (input) => {
+  const urlifiedStringParts = [];
+  let writePosition = input.length - 1;
+  let readPosition =
+      indexOfLastNonWhiteSpaceCharacter(input, input.length);
+  if (readPosition === 0) {
+    writePosition = 0;
+    while (writePosition < input.length) {
+      urlifiedStringParts.push('%20');
+      writePosition += 3;
+    }
+  } else {
+    while (readPosition >= 0) {
+      if (input[readPosition] !== ' ') {
+        urlifiedStringParts[writePosition] = input[readPosition];
+        writePosition--;
+      } else {
+        urlifiedStringParts[writePosition--] = '0';
+        urlifiedStringParts[writePosition--] = '2';
+        urlifiedStringParts[writePosition--] = '%';
+      }
+      readPosition--;
+    }
+  }
+  return urlifiedStringParts.join('');
+};
---
+++ js/src/arrays-and-strings/urlify.test.js
@@ -0,0 +1,38 @@
+import { cStyleUrlify, urlify } from './urlify';
+
+describe('cStyleUrlify', () => {
+
+  test('returns char array string for empty char array', () => {
+    expect(cStyleUrlify([], 0)).toStrictEqual([]);
+  });
+
+  test.each([
+        [['M','r',' ','J','o','h','n',' ','S','m','i','t','h',' ',' ',' ',' '],
+         ['M','r','%','2','0','J','o','h','n','%','2','0','S','m','i','t','h']],
+        [['S',' ','p',' ','a',' ','c',' ','e',' ',' ',' ',' ',' ',' ',' ',' '],
+         ['S','%','2','0','p','%','2','0','a','%','2','0','c','%','2','0','e']],
+        [[' ',' ',' ',' ',' ',' '], ['%','2','0','%','2','0']],
+      ])('returns urlified char array for input', (input, urlifiedInput) => {
+    expect(cStyleUrlify(input, input.length)).toStrictEqual(urlifiedInput);
+  });
+
+});
+
+describe('urlify', () => {
+
+  test('returns empty string for empty string', () => {
+    expect(urlify('')).toBe('');
+  });
+
+  test.each([
+        ['Mr John Smith    ',
+         'Mr%20John%20Smith'],
+        ['S p a c e        ',
+         'S%20p%20a%20c%20e'],
+        ['      ', '%20%20'],
+        ['Extra  spaces  inside        ',
+         'Extra%20%20spaces%20%20inside']
+      ])('returns urlified string for input', (input, urlifiedInput) => {
+    expect(urlify(input)).toBe(urlifiedInput);
+  });
+});
