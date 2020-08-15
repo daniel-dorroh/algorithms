024076137857fb6dab2b3b86dd4c6d5828dad873
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-14 22:39:47 -0500

    Completed solution for palindrome permutation

---
+++ js/src/arrays-and-strings/1.4-palindrome-permutation.js
@@ -0,0 +1,42 @@
+// Given a string, write a function to check
+// if it is a permutation of a palindrome.
+// A palindrome is a word or phrase that is the
+// same forwards and backwards. A permutation is
+// a rearrangement of letters. The palindrome does
+// not need to be limited to just dictionary words.
+
+// Example
+//  Input: 'Tact Coa'
+//  Output: true
+// Remarks
+//  The reason 'Tact Coa' is valid is that it can make the following
+//  palindromes: 'taco cat', 'atco cta', and so on.
+//  It is apparent that white space does not factor into the
+//  palindrome-ness of the input. Otherwise, we would need
+//  an extra white space to make 'Tact Coa' a valid input.
+
+const isOdd = (input) => (input % 2) !== 0;
+
+export const hasPalindromePermutation = (input) => {
+  const charCountInfo = {};
+  let oddCharGroupCount = 0;
+  for (let char of input) {
+    // White space is not counted based on example.
+    // See previous remarks
+    if (char === ' ') {
+      continue;
+    }
+    if (charCountInfo[char] === undefined) {
+      charCountInfo[char] = 1;
+      oddCharGroupCount++;
+    } else {
+      charCountInfo[char]++;
+      if (isOdd(charCountInfo[char])) {
+        oddCharGroupCount++;
+      } else {
+        oddCharGroupCount--;
+      }
+    }
+  }
+  return oddCharGroupCount <= 1;
+};
---
+++ js/src/arrays-and-strings/1.4-palindrome-permutation.test.js
@@ -0,0 +1,33 @@
+import { hasPalindromePermutation } from './1.4-palindrome-permutation';
+
+describe('hasPalindromePermutation', () => {
+
+  test.each([
+        '1234567',
+        'abcdefg',
+        '1  2',
+        '1abc1',
+        '   21',
+      ])('false for strings without a palindrome permutation', (input) => {
+    expect(hasPalindromePermutation(input)).toBe(false);
+  });
+
+  test.each([
+        'aaa',
+        'rraacce',
+        '1  1',
+        '1212   ',
+        'abab',
+        'aabbccc   ',
+        ' ',
+        '   ',
+        'aabb',
+        'ab a b',
+        ' a b a b ',
+        'sasadfgsadfghjk;hjk;sadfghjk;dfghjk;',
+        'sa sadfgsadfgh jk;hjkz;sadfg hjk;dfghjk;',
+      ])('true for strings with a palindrome permutation', (input) => {
+    expect(hasPalindromePermutation(input)).toBe(true);
+  });
+
+});
