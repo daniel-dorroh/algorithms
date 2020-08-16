fc0d94a47f2b83cad88505dfd86fed86307c373c
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-16 11:57:20 -0500

    extracted function to refactor the insertion search

--- js/src/arrays-and-strings/1.5-one-away.js
+++ js/src/arrays-and-strings/1.5-one-away.js
@@ -3,12 +3,30 @@
 // or replace a character. Given two strings, write a
 // function to check if they are one edit or zero edits away.
 
+const searchForInsertions = (longer, shorter, lIndex, sIndex, insertionCount) => {
+  const maxInsertionCount = insertionCount;
+  insertionCount--;
+  lIndex++;
+  while (insertionCount >= 0) {
+    if (longer[lIndex] === shorter[sIndex]) {
+      return {
+        differenceCount: maxInsertionCount - insertionCount,
+        newLIndex: lIndex,
+        newMaxInsertionCount: insertionCount,
+      };
+    }
+    insertionCount--;
+    lIndex++;
+  }
+  return null;
+}
+
 export const diffInputs = (longer, sameOrShorter) => {
   if (longer.length < sameOrShorter.length) {
     throw `longer input '${longer}' is shorter than sameOrShorter input '${sameOrShorter}'`;
   }
   let differenceCount = 0;
-  let possibleInsertionCount = longer.length - sameOrShorter.length;
+  let maxInsertionCount = longer.length - sameOrShorter.length;
   for (let s = 0, l = 0; l < longer.length; s++, l++) {
     if (s === sameOrShorter.length) {
       differenceCount += longer.length - l;
@@ -17,25 +35,16 @@ export const diffInputs = (longer, sameOrShorter) => {
     // A difference of some kind exists here.
     if (longer[l] !== sameOrShorter[s]) {
       // A char was inserted if an insertion is possible
-      // and if the next char in the longer string matches
+      // and if a subsequent char in the longer string matches
       // the current char in the shorter string. Once the
       // number of possible insertions is exhausted, the
       // only remaining possible edit is a mutation.
       let iterationDifferenceCount = 1;
-      let searchingInsertionCount = possibleInsertionCount - 1;
-      let searchingL = l + 1;
-      // Search to see if change is an insertion.
-      while (searchingInsertionCount >= 0) {
-        if (longer[searchingL] === sameOrShorter[s]) {
-          // Since the change was an insertion, record
-          // how many characters were inserted by updating
-          // loop indices.
-          l = searchingL;
-          iterationDifferenceCount = possibleInsertionCount - searchingInsertionCount;
-          possibleInsertionCount = searchingInsertionCount;
-        }
-        searchingInsertionCount--;
-        searchingL++;
+      const results = searchForInsertions(longer, sameOrShorter, l, s, maxInsertionCount);
+      if (results) {
+        iterationDifferenceCount = results.differenceCount;
+        l = results.newLIndex;
+        maxInsertionCount = results.newMaxInsertionCount;
       }
       differenceCount += iterationDifferenceCount;
     }
