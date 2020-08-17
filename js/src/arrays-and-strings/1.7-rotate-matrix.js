dfe77c9c802e5aace8f2d8cfc0e4cfeef3634c97
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-16 22:40:13 -0500

    implemented an in-place alternative to matrix rotation

--- js/src/arrays-and-strings/1.7-rotate-matrix.js
+++ js/src/arrays-and-strings/1.7-rotate-matrix.js
@@ -4,28 +4,70 @@
 // to rotate the image by 90 degrees.
 // Can you do this in place?
 
+const createEmptySquareMatrix = (length) => {
+  const squareMatrix = [];
+  for (let i = 0; i < length; i++) {
+    squareMatrix[i] = [];
+  }
+  return squareMatrix;
+};
+
 export const rotateImage = (image) => {
   if (!Array.isArray(image)) {
     throw 'image is not an array';
   }
-  if (image.length === 0) {
+  const length = image.length;
+  if (length === 0) {
     return image;
   }
-  if (image.length !== image[0].length) {
+  if (length !== image[0].length) {
     throw 'image is not a square matrix';
   }
-  if (image.length === 1) {
+  if (length === 1) {
     return image;
   }
-  const rotatedImage = [];
-  for (let k = 0; k < image.length; k++) {
-    rotatedImage[k] = [];
-  }
-  for (let i = 0; i < image.length; i++) {
-    const rotatedJ = image.length - 1 - i;
-    for (let j = 0; j < image.length; j++) {
+  const rotatedImage = createEmptySquareMatrix(length);
+  for (let i = 0; i < length; i++) {
+    const rotatedJ = length - 1 - i;
+    for (let j = 0; j < length; j++) {
       rotatedImage[j][rotatedJ] = image[i][j];
     }
   }
   return rotatedImage;
+};
+
+export const inPlaceRotateImage = (image) => {
+  if (!Array.isArray(image)) {
+    throw 'image is not an array';
+  }
+  const length = image.length;
+  if (length === 0) {
+    return image;
+  }
+  if (length !== image[0].length) {
+    throw 'image is not a square matrix';
+  }
+  if (length === 1) {
+    return image;
+  }
+  // First, rotate on the left->right, bottom->top diagonal
+  for (let i = 0, k = length; i < length; k--, i++) {
+    for (let j = 0; j < k; j++) {
+      const temp = image[i][j];
+      const rotatedI = length - j - 1;
+      const rotatedJ = length - i - 1;
+      image[i][j] = image[rotatedI][rotatedJ];
+      image[rotatedI][rotatedJ] = temp;
+    }
+  }
+  // Second, rotate on horizontal
+  for (let i = 0; i < Math.floor(length / 2); i++) {
+    for (let j = 0; j < length; j++) {
+      const rotatedI = length - i - 1;
+      const temp = image[i][j];
+      image[i][j] = image[rotatedI][j];
+      image[rotatedI][j] = temp;
+    }
+  }
+  return image;
 };
--- js/src/arrays-and-strings/1.7-rotate-matrix.test.js
+++ js/src/arrays-and-strings/1.7-rotate-matrix.test.js
@@ -1,48 +1,58 @@
-import { rotateImage } from './1.7-rotate-matrix';
+import { rotateImage, inPlaceRotateImage } from './1.7-rotate-matrix';
 
-describe('rotateImage', () => {
+describe('rotateImage and inPlaceRotateImage', () => {
 
   test.each([null, undefined, 123, 'twenty-five', {}, () => true])('throws if image is not an array', (image) => {
     expect(() => rotateImage(image)).toThrow('image is not an array');
+    expect(() => inPlaceRotateImage(image)).toThrow('image is not an array');
   });
 
   test('returns image if image is empty', () => {
     expect(rotateImage([])).toStrictEqual([]);
+    expect(inPlaceRotateImage([])).toStrictEqual([]);
   });
 
   test('throws if image is not a square matrix', () => {
     expect(() => rotateImage([[1,2],[3,4],[5,6]])).toThrow('image is not a square matrix');
+    expect(() => inPlaceRotateImage([[1,2],[3,4],[5,6]])).toThrow('image is not a square matrix');
   });
 
   test('returns image if image is 1x1', () => {
     const image = [[25]];
     expect(rotateImage(image)).toStrictEqual(image);
+    expect(inPlaceRotateImage(image)).toStrictEqual(image);
   });
 
   test('rotates a 2x2 matrix', () => {
-    expect(rotateImage([
-          [1, 2],
-          [3, 4],
-        ])).toStrictEqual([
-          [3, 1],
-          [4, 2],
-        ]);
+    const image = [
+      [1, 2],
+      [3, 4],
+    ];
+    const rotatedImage = [
+      [3, 1],
+      [4, 2],
+    ];
+    expect(rotateImage(image)).toStrictEqual(rotatedImage);
+    expect(inPlaceRotateImage(image)).toStrictEqual(rotatedImage);
   });
 
   test('rotates a 5x5 matrix', () => {
-    expect(rotateImage([
-          [1,   2,  3,  4,  5],
-          [6,   7,  8,  9, 10],
-          [11, 12, 13, 14, 15],
-          [16, 17, 18, 19, 20],
-          [21, 22, 23, 24, 25],
-        ])).toStrictEqual([
-          [21, 16, 11,  6,  1],
-          [22, 17, 12,  7,  2],
-          [23, 18, 13,  8,  3],
-          [24, 19, 14,  9,  4],
-          [25, 20, 15, 10,  5],
-        ]);
+    const image = [
+      [ 1,  2,  3,  4,  5],
+      [ 6,  7,  8,  9, 10],
+      [11, 12, 13, 14, 15],
+      [16, 17, 18, 19, 20],
+      [21, 22, 23, 24, 25],
+    ];
+    const rotatedImage = [
+      [21, 16, 11,  6,  1],
+      [22, 17, 12,  7,  2],
+      [23, 18, 13,  8,  3],
+      [24, 19, 14,  9,  4],
+      [25, 20, 15, 10,  5],
+    ];
+    expect(rotateImage(image)).toStrictEqual(rotatedImage);
+    expect(inPlaceRotateImage(image)).toStrictEqual(rotatedImage);
   });
 
 });
