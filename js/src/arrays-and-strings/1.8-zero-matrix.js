5ce10e151f95c4f0eaf5aca1c3cd6a8ce7796438
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-17 19:42:57 -0500

    Made solution into a bomberman simulation

--- js/src/arrays-and-strings/1.8-zero-matrix.js
+++ js/src/arrays-and-strings/1.8-zero-matrix.js
@@ -4,7 +4,151 @@ import { throwIfNotArray } from '../utility/arg-checking';
 // an element in an MxN matrix is 0,
 // its entire row and column are set to 0;
 
-export const bombermanTheMatrix = (input) => {
+// Note: I went a bit crazy with the
+// solution for this one. The prompt
+// reminded me of the old bomberman
+// games, so I decided to solve it as
+// though it were a game. Rather than
+// the bombs ticking down and going off,
+// the bombs go off if they're visited
+// or if an explosion reaches them.
+// You can enable the visualization by
+// passing true to the constructor of
+// the MapController.
+
+class MapController {
+
+  constructor(map, logToConsole = false) {
+    this.map_ = map;
+    this.length_ = map.length;
+    this.width_ = map[0].length;
+    this.burntTiles_ = [];
+    this.explosionVectors_ = [];
+    this.logToConsole_ = logToConsole;
+    this.lastCheckedTile_ = [0, 0];
+  }
+
+  processTile(i, j) {
+    if (this.logToConsole_) {
+      this.lastCheckedTile_ = [i, j];
+    }
+    if (this.isTileBurnt_(i, j)) {
+      return;
+    }
+    if (!this.isTileBomb_(i, j)) {
+      return;
+    }
+    this.burnTile_(i, j);
+  }
+
+  processExplosions() {
+    const finishedExplosions = [];
+    for (const explosion of [...this.explosionVectors_]) {
+      if (explosion.done(explosion.next)) {
+        finishedExplosions.push(explosion);
+        continue;
+      }
+      const [nextI, nextJ] = explosion.next;
+      this.burnTile_(nextI, nextJ);
+      explosion.next = explosion.step(explosion.next);
+    }
+    for (const explosion of finishedExplosions) {
+      this.explosionVectors_.splice(this.explosionVectors_.indexOf(explosion), 1);
+    }
+    if (this.logToConsole_) {
+      this.printMap_();
+    }
+  }
+
+  finish() {
+    while (this.explosionVectors_.length > 0) {
+      this.processExplosions();
+    }
+  }
+
+  isTileBomb_(i, j) {
+    return this.map_[i][j] === 0;
+  }
+
+  isTileBurnt_(i, j) {
+    return this.burntTiles_[i] !== undefined
+        && this.burntTiles_[i][j] === true;
+  }
+
+  burnTile_(i, j) {
+    if (this.isTileBurnt_(i, j)) {
+      return;
+    }
+    if (this.burntTiles_[i] === undefined) {
+      this.burntTiles_[i] = [];
+    }
+    this.burntTiles_[i][j] = true;
+    if (this.isTileBomb_(i, j)) {
+      this.startExplosion_(i, j);
+    }
+    this.map_[i][j] = 0;
+    return;
+  }
+
+  startExplosion_(originI, originJ) {
+    if (originI - 1 >= 0 && !this.isTileBurnt_(originI - 1, originJ)) {
+      this.explosionVectors_.push({
+        next: [originI - 1, originJ],
+        done: ([i, j]) => i < 0,
+        step: ([i, j]) => [--i, j]
+      });
+    }
+    if (originI + 1 < this.length_ && !this.isTileBurnt_(originI + 1, originJ)) {
+      this.explosionVectors_.push({
+        next: [originI + 1, originJ],
+        done: ([i, j]) => i >= this.length_,
+        step: ([i, j]) => [++i, j]
+      });
+    }
+    if (originJ - 1 >= 0 && !this.isTileBurnt_(originI, originJ - 1)) {
+      this.explosionVectors_.push({
+        next: [originI, originJ - 1],
+        done: ([i, j]) => j < 0,
+        step: ([i, j]) => [i, --j]
+      });
+    }
+    if (originJ + 1 < this.width_ && !this.isTileBurnt_(originI, originJ + 1)) {
+      this.explosionVectors_.push({
+        next: [originI, originJ + 1],
+        done: ([i, j]) => j >= this.width_,
+        step: ([i, j]) => [i, ++j]
+      });
+    }
+  }
+
+  printMap_() {
+    const printRows = [];
+    for (let i = 0; i < this.length_; i++) {
+      const row = [...this.map_[i]];
+      const friedRow = this.burntTiles_[i];
+      row = friedRow === undefined
+          ? row.map(c => c === 0 ? 'BOMB' : c)
+          : row.map((c, rI) => c !== 0
+              ? c
+              : friedRow[rI]
+                  ? 'EXPLOSION'
+                  : 'BOMB');
+      if (this.lastCheckedTile_[0] === i) {
+        let currentTile = row[this.lastCheckedTile_[1]];
+        if (currentTile !== 'BOMB' && currentTile !== 'EXPLOSION') {
+          row[this.lastCheckedTile_[1]] = '✓';
+        }
+      }
+      printRows.push(row.join('  '));
+    }
+    console.log(printRows.join('\n')
+        .replace(/ ?EXPLOSION/g, '💥')
+        .replace(/ ?BOMB/g, '💣'));
+  }
+
+}
+
+export const bombermanTheMap = (input) => {
   throwIfNotArray(input);
   const length = input.length;
   if (length === 0) {
@@ -14,31 +158,13 @@ export const bombermanTheMatrix = (input) => {
   if (width === 0) {
     return input;
   }
-  const zeroedRows = [];
-  const zeroedColumns = [];
+  const controller = new MapController(input, /*, true*/);
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < width; j++) {
-      if (input[i][j] === 0) {
-        zeroedRows[i] = true;
-        zeroedColumns[j] = true;
-      }
+      controller.processTile(i, j);
+      controller.processExplosions();
     }
   }
-  for (let i = 0; i < zeroedRows.length; i++) {
-    if (zeroedRows[i] === undefined) {
-      continue;
-    }
-    for (let j = 0; j < width; j++) {
-      input[i][j] = 0;
-    }
-  }
-  for (let j = 0; j < zeroedColumns.length; j++) {
-    if (zeroedColumns[j] === undefined) {
-      continue;
-    }
-    for (let i = 0; i < length; i++) {
-      input[i][j] = 0;
-    }
-  }
+  controller.finish();
   return input;
 };
--- js/src/arrays-and-strings/1.8-zero-matrix.test.js
+++ js/src/arrays-and-strings/1.8-zero-matrix.test.js
@@ -1,34 +1,34 @@
-import { bombermanTheMatrix } from './1.8-zero-matrix';
+import { bombermanTheMap } from './1.8-zero-matrix';
 
 describe('bombermanZeros', () => {
 
   test.each([123, 'twenty-five', {}, () => true])('throws if input is not an array', (input) => {
-    expect(() => bombermanTheMatrix(input)).toThrow('input is not an array');
+    expect(() => bombermanTheMap(input)).toThrow('input is not an array');
   });
 
   test('returns input if empty array', () => {
-    expect(bombermanTheMatrix([])).toStrictEqual([]);
+    expect(bombermanTheMap([])).toStrictEqual([]);
   });
 
   test('returns input if 0x0 matrix', () => {
-    expect(bombermanTheMatrix([[]])).toStrictEqual([[]]);
+    expect(bombermanTheMap([[]])).toStrictEqual([[]]);
   });
 
   test('returns input if 1x1 matrix', () => {
     const input = [[25]];
-    expect(bombermanTheMatrix(input)).toStrictEqual(input);
+    expect(bombermanTheMap(input)).toStrictEqual(input);
   });
 
   test('bombermans all entries of a 1xN matrix', () => {
     const input = [[1, 2, 3, 4, 0, 6, 7, 8]];
     const bombermannedInput = [[0, 0, 0, 0, 0, 0, 0, 0]];
-    expect(bombermanTheMatrix(input)).toStrictEqual(bombermannedInput);
+    expect(bombermanTheMap(input)).toStrictEqual(bombermannedInput);
   });
 
   test('bombermans all entries of a Nx1 matrix', () => {
     const input = [[1], [2], [3], [4], [0], [6], [7], [8]];
     const bombermannedInput = [[0], [0], [0], [0], [0], [0], [0], [0]];
-    expect(bombermanTheMatrix(input)).toStrictEqual(bombermannedInput);
+    expect(bombermanTheMap(input)).toStrictEqual(bombermannedInput);
   });
 
   test('bombermans expected rows and columns', () => {
@@ -58,7 +58,7 @@ describe('bombermanZeros', () => {
       [1, 0, 3, 4, 0, 6, 7, 0, 9],
       [0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
-    expect(bombermanTheMatrix(input)).toStrictEqual(bombermannedInput);
+    expect(bombermanTheMap(input)).toStrictEqual(bombermannedInput);
   });
 
   test('makes all zero matrix', () => {
@@ -76,7 +76,7 @@ describe('bombermanZeros', () => {
       [0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0],
     ];
-    expect(bombermanTheMatrix(input)).toStrictEqual(bombermannedInput);
+    expect(bombermanTheMap(input)).toStrictEqual(bombermannedInput);
   });
 
 });
