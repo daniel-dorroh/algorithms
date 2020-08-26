527384a9952031028d236ffc2ca7ddf5017a01d7
daniel-dorroh <daniel.dorroh@gmail.com>
2020-08-26 18:05:06 -0500

    Wrote solution for list of depths

---
+++ js/src/4-trees-and-graphs/4.3-list-of-depths.js
@@ -0,0 +1,32 @@
+
+// Given a binary tree, design an algorithm
+// that creates linked lists of all the
+// nodes at each depth.
+
+const processDepth = (nodes, atDepthNodes, depth) => {
+  nodes[depth] = [];
+  const nextAtDepthNodes = [];
+  for (const node of atDepthNodes) {
+    nodes[depth].push(node);
+    const left = node.left();
+    if (left !== null) {
+      nextAtDepthNodes.push(left);
+    }
+    const right = node.right();
+    if (right !== null) {
+      nextAtDepthNodes.push(right);
+    }
+  }
+  if (nextAtDepthNodes.length > 0) {
+    processDepth(nodes, nextAtDepthNodes, depth + 1);
+  }
+};
+
+export const getNodesByDepth = (root) => {
+  const nodes = [];
+  if (root === null || root === undefined) {
+    return nodes;
+  }
+  processDepth(nodes, [root], 0);
+  return nodes;
+};
---
+++ js/src/4-trees-and-graphs/4.3-list-of-depths.test.js
@@ -0,0 +1,40 @@
+import { Node, toBinarySearchTree } from './4.2-minimal-tree';
+import { getNodesByDepth } from './4.3-list-of-depths';
+
+describe('getNodesByDepth', () => {
+
+  test.each([null, undefined])('returns empty list for null or undefined root', (root) => {
+    expect(getNodesByDepth(root)).toStrictEqual([]);
+  });
+
+  test('returns list of one list containing node for single node tree', () => {
+    const root = new Node(25);
+    const nodes = getNodesByDepth(root);
+    expect(nodes).toHaveLength(1);
+    expect(nodes[0][0]).toStrictEqual(root);
+  });
+
+  test('returns list of two lists for two level tree', () => {
+    const root = new Node(25);
+
+    const children = [new Node(35), new Node(45)];
+    root.addLeft(children[0]);
+    root.addRight(children[1]);
+    const nodes = getNodesByDepth(root);
+    expect(nodes).toHaveLength(2);
+    expect(nodes[0][0]).toStrictEqual(root);
+    expect(nodes[1]).toStrictEqual(children);
+  });
+
+  test('returns the correct list of node lists', () => {
+    const root = toBinarySearchTree([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]);
+    const nodes = getNodesByDepth(root);
+    expect(nodes).toHaveLength(5);
+    expect(nodes[0]).toHaveLength(1);
+    expect(nodes[1]).toHaveLength(2);
+    expect(nodes[2]).toHaveLength(4);
+    expect(nodes[3]).toHaveLength(7);
+    expect(nodes[4]).toHaveLength(8);
+  });
+
+});
