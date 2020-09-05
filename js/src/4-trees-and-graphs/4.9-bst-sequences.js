bb1d7079f6f910cf2a5654a238cec194ac319a9c
daniel-dorroh <daniel.dorroh@gmail.com>
2020-09-05 11:49:28 -0500

    Refactored bst sequences and added comments

--- js/src/4-trees-and-graphs/4.9-bst-sequences.js
+++ js/src/4-trees-and-graphs/4.9-bst-sequences.js
@@ -1,3 +1,4 @@
+import { throwIfNotBinarySearchTree } from './binary-search-tree';
 
 // A binary search tree was created by traversing
 // through an array from left to right and
@@ -5,49 +6,60 @@
 // tree with distinct elements, print all possible
 // arrays that could have led to this tree.
 
-import { throwIfNotBinarySearchTree } from './binary-search-tree';
-
-const getPossibleInOrderCombinations = (leftSource, rightSource) => {
-  const getCombinations = (left, right, combinations, combination = []) => {
-    if (left.length === 0 || right.length === 0) {
-      combinations.push(combination.concat(left.concat(right)));
-      return;
-    }
-    const modifiedLeft = [...left];
-    let modifiedCombination = [...combination];
-    const firstLeft = modifiedLeft.shift();
-    modifiedCombination.push(firstLeft);
-    getCombinations(modifiedLeft, right, combinations, modifiedCombination);
-    const modifiedRight = [...right];
-    modifiedCombination = [...combination];
-    const firstRight = modifiedRight.shift();
-    modifiedCombination.push(firstRight);
-    getCombinations(left, modifiedRight, combinations, modifiedCombination);
-  };
-  const sourceCombinations = [];
-  getCombinations(leftSource, rightSource, sourceCombinations);
-  return sourceCombinations;
+const getOrderPreservingCombinations = (left, right, combination = []) => {
+  if (left.length === 0 || right.length === 0) {
+    return [combination.concat(left.concat(right))];
+  }
+  const nextLeft = [...left];
+  const nextLeftCombination = [...combination, nextLeft.shift()];
+  const leftCombinations = getOrderPreservingCombinations(nextLeft, right, nextLeftCombination);
+  const nextRight = [...right];
+  const nextRightCombination = [...combination, nextRight.shift()];
+  const rightCombinations = getOrderPreservingCombinations(left, nextRight, nextRightCombination);
+  return [...leftCombinations, ...rightCombinations];
 };
 
 const getAllSourceArrays = (node) => {
-  const source = [];
-  source.push(node.value());
-  while (node.left() === null || node.right() === null) {
+  const source = [node.value()];
+  // Move down single-child regions of the tree
+  // Return the source node sequence if a leaf is encountered
+  while (node.childCount() <= 1) {
     if (node.left() !== null) {
       node = node.left();
     } else if (node.right() !== null) {
       node = node.right();
     } else {
+      // Single-child node sequence terminated
+      // in leaf, meaning the only source array
+      // is the single-child node sequence.
       return [source];
     }
     source.push(node.value());
   }
+  // Left and right child sequence order does not matter
+  // relative to each other, so all possible source arrays
+  // are the combination of left and right sequences mixed
+  // together, preserving the order relative to each sequence itself.
+  // Ex: [1,2,3] and [6] could have resulted from the following source arrays
+  // Src: [1,2,3,6], [1,2,6,3], [1,6,2,3], [6,1,2,3]
+  // However, notice that the relative ordering of 1, 2, and 3
+  // remains the same. This is not a permutation.
+  // If the left/right split was preceded by a single-child
+  // node sequence, each combination must be preceded by this
+  // sequence.
+  // Ex:  5
+  //       6
+  //      7 8
+  //    ... ...
+  // The combination of subsequences starting at 7 and 8 must all
+  // be preceded by [5,6] because there is no other path to those
+  // subsequences.
   const sourceArrays = [];
   const leftSourceArrays = getAllSourceArrays(node.left());
   const rightSourceArrays = getAllSourceArrays(node.right());
-  for (const leftSource of leftSourceArrays) {
-    for (const rightSource of rightSourceArrays) {
-      for (const combination of getPossibleInOrderCombinations(leftSource, rightSource)) {
+  for (const left of leftSourceArrays) {
+    for (const right of rightSourceArrays) {
+      for (const combination of getOrderPreservingCombinations(left, right)) {
         sourceArrays.push([...source, ...combination]);
       }
     }
@@ -57,6 +69,5 @@ const getAllSourceArrays = (node) => {
 
 export const findSourceArrays = (bst) => {
   throwIfNotBinarySearchTree(bst);
-  const root = bst.root();
-  return getAllSourceArrays(root);
+  return getAllSourceArrays(bst.root());
 };
